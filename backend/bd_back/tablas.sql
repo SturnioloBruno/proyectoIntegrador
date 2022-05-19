345494b14999d92a697d60dae4e3b3ee48ed43fa
Ana Olivares <anaolivares2712@gmail.com>
2022-05-18 22:01:19 -0300

    [ADD] tabla categoria

---
+++ backend/bd_back/tablas.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS categoria (
+	cat_id INT AUTO_INCREMENT PRIMARY KEY,
+    cat_titulo VARCHAR(100) NOT NULL, 
+    cat_descripcion VARCHAR(500),
+    cat_url_imagen VARCHAR(500) NOT NULL
+);
+
+

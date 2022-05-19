166967e0e29e846bbbe1809f0d3e5f8730ea0226
gia-developer <gianna.belen.russo@gmail.com>
2022-05-18 23:27:19 -0300

    [FIX] App & Section

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
 import React, {Fragment} from 'react';
 import Header from './components/Header';
 import Search from './components/Search';
-import Card from './components/cards/Card';
 import Footer from './components/Footer';
 import Section from './components/Section';
 
@@ -11,14 +10,8 @@ function App() {
       <Header />
       <Search />
 
-      <Section title="Buscar por tipo de alojamiento" />
-      <section>
-        <h2>Recomendaciones</h2>
-        <ul>
-          <li><Card title="Hermitage Hotel" /></li>
-          <li><Card title="Hermitage Hotel" /></li>
-        </ul>
-      </section>
+      <Section title="Buscar por tipo de alojamiento" type="Category" />
+      <Section title="Recomendaciones" type="Card" />
 
       <Footer />
     </Fragment>
--- frontend/src/components/Section.js
+++ frontend/src/components/Section.js
@@ -1,14 +1,31 @@
+import React, {Fragment} from 'react';
 import Category from './cards/Category';
+import Card from '../components/cards/Card';
 
 function Section(props) {
+    const type = props.type;
     return (
         <section>
             <h2>{props.title}</h2>
             <ul>
-                <li><Category title="Hoteles" /></li>
-                <li><Category title="Hotels" /></li>
-                <li><Category title="Departamentos" /></li>
-                <li><Category title="Bed and Breakfast" /></li>
+                {type == "Category" &&
+                <Fragment>
+                    {// Acá va el for con la BBDD de las categorías
+                    }
+                    <li><Category title="Hoteles" /></li>
+                    <li><Category title="Hotels" /></li>
+                    <li><Category title="Departamentos" /></li>
+                    <li><Category title="HotBed and breakfasteles" /></li>
+                </Fragment>
+                }
+                {type == "Card" &&
+                <Fragment>
+                    {// Acá va el for con la BBDD de los nombres de hoteles
+                    }
+                    <li><Card title="Hermitage Hotel" /></li>
+                    <li><Card title="Hermitage Hotel" /></li>
+                </Fragment>
+                }
             </ul>
         </section>
     )

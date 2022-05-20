0b287f3ac0a3d45889d4e8c1ceddd3795948c8cc
nicolasmartingutierrez@gmail.com <nicolasmartingutierrez@gmail.com>
2022-05-20 12:20:54 -0300

    [DELETE] Carpeta back

--- backend/bd_back/tablas.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS categoria (
-	cat_id INT AUTO_INCREMENT PRIMARY KEY,
-    cat_titulo VARCHAR(100) NOT NULL, 
-    cat_descripcion VARCHAR(500),
-    cat_url_imagen VARCHAR(500) NOT NULL
-);
-
-
--- backend/pi_back/proyectointegrador/src/main/java/com/grupo01/proyectointegrador/Exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.grupo01.proyectointegrador.Exception;
-
-public class ResourceNotFoundException extends Exception {
-    public ResourceNotFoundException(String msj){
-        super(msj);
-    }
-}

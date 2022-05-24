2a4e884aba81ae418fb06869668a3b5009101503
Ana Olivares <anaolivares2712@gmail.com>
2022-05-24 18:38:34 -0300

    [ADD] map alojamientos

--- frontend/src/components/Section.js
+++ frontend/src/components/Section.js
@@ -2,16 +2,14 @@ import Category from './cards/Category';
 import Card from '../components/cards/Card';
 import '../styles/Section.css';
 import Categories from "../data/categories.json";
+import Alojamientos from "../data/alojamiento.json";
 
 function Section({ type, title, src }) {
-    console.log(Categories)
     return (
         <section>
             <h2>{title}</h2>
             {type == "Category" &&
             <ul className="ul__categories-list">
-                {// Acá va el for con la BBDD de las categorías
-                }
                 {Categories.map((cat) => (
                     <li><a href='#'><Category title={cat.cat_titulo} src={cat.cat_url_img} /></a></li>
                 ))}
@@ -19,12 +17,9 @@ function Section({ type, title, src }) {
             }
             {type == "Card" &&
             <ul className="ul__accommodation-list">
-                {// Acá va el for con la BBDD de los nombres de hoteles
-                }
-                <li><a href='#'><Card title="Hermitage Hotel" /></a></li>
-                <li><a href='#'><Card title="Hermitage Hotel" /></a></li>
-                <li><a href='#'><Card title="Hermitage Hotel" /></a></li>
-                <li><a href='#'><Card title="Hermitage Hotel" /></a></li>
+                {Alojamientos.map((alo) => (
+                    <li><a href='#'><Card title={alo.alo_titulo} /></a></li>
+                ))}
             </ul>
             }
         </section>

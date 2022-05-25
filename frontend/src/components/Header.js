8c5d301e5b7bb48204e12fd056558bae96baefd7
gia-developer <gianna.belen.russo@gmail.com>
2022-05-25 15:17:47 -0300

    [FIX] Menú header + Login

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,8 +1,5 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
 import Title from "./Title";
-import Login from "./login/Login";
-import Register from "./login/Register";
 import '../styles/Header.css';
 
 function Header() {
@@ -16,19 +13,30 @@ function Header() {
             <div>
                 <a href="#" className="a__button-nav" onClick={toggle}>Abrir/Cerrar</a>
                 <div className="div__menu-bar">
-                    <p>Menú</p>
-                    <nav>
-                        <ul className="ul__bar-links">
-                            <li><Link to="/login" element={<Login />}>Iniciar sesión</Link></li>
-                            <li><Link to="/register" element={<Register />}>Crear cuenta</Link></li>
-                        </ul>
-                        <ul className="ul__social-links">
-                            <li><a href="#" className="a__icon-fb">Facebook</a></li>
-                            <li><a href="#" className="a__icon-li">LinkedIn</a></li>
-                            <li><a href="#" className="a__icon-tw">Twitter</a></li>
-                            <li><a href="#" className="a__icon-ig">Instagram</a></li>
-                        </ul>
-                    </nav>
+                    <div className="div__menu-login">
+                        {/*<p>Menú</p>*/}
+                        <div className="div__user-login">
+                            <span>BR</span>
+                            <p>Hola, <span>Bruno Rodríguez</span></p>
+                        </div>
+                    </div>
+                    <div className="div__menu-navigation">{/* Acá se agrega la clase login */}
+                        {/*<nav>
+                            <ul className="ul__bar-links">
+                                <li><Link to="/login" element={<Login />}>Iniciar sesión</Link></li>
+                                <li><Link to="/register" element={<Register />}>Crear cuenta</Link></li>
+                            </ul>
+                        </nav>*/}
+                        <div className="div__social-menu">
+                            <span>¿Deseas <a href="#">cerrar sesión</a>?</span>
+                            <ul className="ul__social-links">
+                                <li><a href="#" className="a__icon-fb">Facebook</a></li>
+                                <li><a href="#" className="a__icon-li">LinkedIn</a></li>
+                                <li><a href="#" className="a__icon-tw">Twitter</a></li>
+                                <li><a href="#" className="a__icon-ig">Instagram</a></li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
             </div>
         </header>

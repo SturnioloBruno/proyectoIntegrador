6816e63e30a5840e30344cf2e1150ac04e02ce8e
gia-developer <gianna.belen.russo@gmail.com>
2022-05-30 19:57:50 -0300

    [ADD] Componentes productos

---
+++ frontend/src/components/products/HeaderProduct.js
@@ -0,0 +1,13 @@
+function HeaderProduct({category, name}) {
+    return (
+        <div className="div__header-product">
+            <h2>Hermitage Hotel</h2>
+            <p>Hotel</p>
+            {/*<h2>${name}</h2>
+            <p>${category}</p>*/}
+            <Link to="#" className="a__show-more" />
+        </div>
+    )
+}
+
+export default HeaderProduct;
---
+++ frontend/src/components/products/InfoProduct.js
@@ -0,0 +1,23 @@
+function InfoProduct({address, meters, score, stars, puntuation}) {
+    return (
+        <div className='div__address-score'>
+            <div className='div__info-address'>
+                <p>Buenos Aires, Ciudad Autónoma de Buenos Aires, Argentina</p>
+                <p>A 940 m del centro</p>
+                {/*<p>${address}</p>
+                <p>A ${meters}m del centro</p>*/}
+            </div>
+            <div className='div__puntuation-score'>
+                <div className="div__info-score">
+                    <p>Muy bueno</p>
+                    {/*<p>${score}</p>*/}
+                    {/*Mostrar estrellas*/}
+                </div>
+                <span>8</span>
+                {/*<span>${puntuation}</span>*/}
+            </div>
+        </div>
+    )
+}
+
+export default InfoProduct;

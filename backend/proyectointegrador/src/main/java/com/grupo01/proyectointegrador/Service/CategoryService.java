5295ae10a13c6ce6c821f53ae9716df23adfa2a6
Ana Olivares <anaolivares2712@gmail.com>
2022-05-31 16:23:21 -0300

    [FIX] CategoryService

--- backend/proyectointegrador/src/main/java/com/grupo01/proyectointegrador/Service/CategoryService.java
+++ backend/proyectointegrador/src/main/java/com/grupo01/proyectointegrador/Service/CategoryService.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-public class CategoriaService {
+public class CategoryService {
 
     @Autowired
     ICategoryRepository categoryRepository;

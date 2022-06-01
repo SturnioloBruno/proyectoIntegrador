e4b54206a979045f316d0450c564516161936c84
Ana Olivares <anaolivares2712@gmail.com>
2022-06-01 18:48:04 -0300

    [ADD] Agrego relaciones

--- backend/proyectointegrador/src/main/java/com/grupo01/proyectointegrador/Model/Product.java
+++ backend/proyectointegrador/src/main/java/com/grupo01/proyectointegrador/Model/Product.java
@@ -3,11 +3,13 @@ package com.grupo01.proyectointegrador.Model;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 @Table(name="products")
 public class Product {
     @Id
+    @Column(name = "prod_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
@@ -36,6 +38,33 @@ public class Product {
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "city_id", nullable = false)
     private City city;
+
+    @OneToMany(mappedBy = "product",fetch = FetchType.EAGER)
+    @JsonIgnore
+    private Set<Product> products;
+
+    public Product(Long id, String name, int puntuation, int stars, String descTitle, String desc, int coordinates, Category category, City city) {
+        this.id = id;
+        this.name = name;
+        this.puntuation = puntuation;
+        this.stars = stars;
+        this.descTitle = descTitle;
+        this.desc = desc;
+        this.coordinates = coordinates;
+        this.category = category;
+        this.city = city;
+    }
+
+    public Product(String name, int puntuation, int stars, String descTitle, String desc, int coordinates, Category category, City city) {
+        this.name = name;
+        this.puntuation = puntuation;
+        this.stars = stars;
+        this.descTitle = descTitle;
+        this.desc = desc;
+        this.coordinates = coordinates;
+        this.category = category;
+        this.city = city;
+    }
 
     public Long getId() {
         return id;

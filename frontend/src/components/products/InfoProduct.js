90d086a91d5495dd8e706103b42d41dc9fb13c92
gia-developer <gianna.belen.russo@gmail.com>
2022-06-01 15:40:07 -0300

    [ADD] #19 - Agregar bloque de imágenes y galería a template producto

--- frontend/src/components/products/InfoProduct.js
+++ frontend/src/components/products/InfoProduct.js
@@ -1,8 +1,13 @@
+import * as React from 'react';
+import Rating from '@mui/material/Rating';
+import Stack from '@mui/material/Stack';
+import StarIcon from '@mui/icons-material/Star';
+
 function InfoProduct({address, meters, score, stars, puntuation}) {
     return (
         <div className='div__address-score'>
             <div className='div__info-address'>
-                <p>Buenos Aires, Ciudad Autónoma de Buenos Aires, Argentina</p>
+                <p className="p__city-info">Buenos Aires, Ciudad Autónoma de Buenos Aires, Argentina</p>
                 <p className="p__center-meters">A 940 m del centro</p>
                 {/*<p>${address}</p>
                 <p>A ${meters}m del centro</p>*/}
@@ -10,8 +15,10 @@ function InfoProduct({address, meters, score, stars, puntuation}) {
             <div className='div__puntuation-score'>
                 <div className="div__info-score">
                     <p>Muy bueno</p>
+                    <Stack spacing={1}>
+                        <Rating name="half-rating-read" defaultValue={2.5} precision={0.5} emptyIcon={<StarIcon style={{ opacity: 0.5 }} />} readOnly />
+                    </Stack>
                     {/*<p>${score}</p>*/}
-                    {/*Mostrar estrellas*/}
                 </div>
                 <span>8</span>
                 {/*<span>${puntuation}</span>*/}

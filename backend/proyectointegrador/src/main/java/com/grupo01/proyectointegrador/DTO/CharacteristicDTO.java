aa701fa8c2530cc3f6c6622e2ec070c89ba62510
nicolasmartingutierrez@gmail.com <nicolasmartingutierrez@gmail.com>
2022-06-02 17:50:47 -0300

    [ADD] PolicyDTO & CharacteristicDTO

---
+++ backend/proyectointegrador/src/main/java/com/grupo01/proyectointegrador/DTO/CharacteristicDTO.java
@@ -0,0 +1,22 @@
+package com.grupo01.proyectointegrador.DTO;
+
+public class CharacteristicDTO {
+    private Long id;
+    private String title;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
---
+++ backend/proyectointegrador/src/main/java/com/grupo01/proyectointegrador/DTO/PolicyDTO.java
@@ -0,0 +1,31 @@
+package com.grupo01.proyectointegrador.DTO;
+
+public class PolicyDTO {
+    private Long id;
+    private String title;
+    private String desc;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+}

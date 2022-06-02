4f74ba410a01e6784f19646111b9e13200dcf82b
gia-developer <gianna.belen.russo@gmail.com>
2022-06-02 00:07:12 -0300

    [ADD] #39 - Incluir calendario de reservas y botón para acceder

--- frontend/src/components/products/HotelDate.js
+++ frontend/src/components/products/HotelDate.js
@@ -1,12 +1,13 @@
 import Button from "../Button";
+import Calendar from "./Calendar";
 
 function HotelDate() {
     return (
         <section className="section__hotel-date">
             <h2>Fechas disponibles</h2>
             <div className="div__calendar-info">
-                {/* Calendario */}
-                <div>
+                <Calendar />
+                <div className="div__button-selected">
                     <p className="p__bold-style">Agregá tus fechas de viaje para obtener precios exactos</p> 
                     <Button text="Iniciar reserva" className="btn button__solid-type" />
                 </div>

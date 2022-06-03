1c7738a7b77794485704cb9acb144847f0c9b553
Ana Olivares <anaolivares2712@gmail.com>
2022-06-03 02:05:42 -0300

    [ADD] Agrego Policy Controller

---
+++ backend/proyectointegrador/src/main/java/com/grupo01/proyectointegrador/Controller/PolicyController.java
@@ -0,0 +1,35 @@
+package com.grupo01.proyectointegrador.Controller;
+
+import com.grupo01.proyectointegrador.Model.Policy;
+import com.grupo01.proyectointegrador.Service.PolicyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+public class PolicyController {
+
+    @Autowired
+    PolicyService policyService;
+
+    @PostMapping("/insert")
+    public ResponseEntity<Policy> guardar(@RequestBody Policy policy) throws Exception {
+        return ResponseEntity.ok(policyService.guardar(policy));
+    }
+
+    @GetMapping("/findById/{id}")
+    public ResponseEntity<Policy> buscarId(@PathVariable Long id) throws Exception {
+        return ResponseEntity.ok(policyService.buscarId(id));
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> borrar(@PathVariable Long id) throws Exception {
+        policyService.borrar(id);
+        return ResponseEntity.ok("Politica borrada con exito");
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<Policy> actualizar(@RequestBody Policy policy) throws Exception {
+        return ResponseEntity.ok(policyService.guardar(policy));
+    }
+
+}

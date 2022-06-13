a5ddc33d54d28265ada2f96f922cd519b5964340
Ana Olivares <anaolivares2712@gmail.com>
2022-06-13 18:25:14 -0300

    [ADD] Agrego Role Model

---
+++ backend/proyectointegrador/src/main/java/com/grupo01/proyectointegrador/Model/Role.java
@@ -0,0 +1,45 @@
+package com.grupo01.proyectointegrador.Model;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id", nullable = false)
+    private Long id;
+
+    @Column (name = "role_name", nullable = false)
+    private String roleName;
+
+    @OneToMany(mappedBy = "roleId", fetch = FetchType.EAGER)
+    private Set<User> users;
+
+    public Role(Long id, String roleName) {
+        this.id = id;
+        this.roleName = roleName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public Set<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Set<User> users) {
+        this.users = users;
+    }
+}

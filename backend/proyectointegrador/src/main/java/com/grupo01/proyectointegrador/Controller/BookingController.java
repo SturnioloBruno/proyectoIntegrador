6483478bc7969fb5ff8b845c788cbb67a66ce4f2
Ana Olivares <anaolivares2712@gmail.com>
2022-06-13 18:27:14 -0300

    [ADD] Agrego Booking Controller

---
+++ backend/proyectointegrador/src/main/java/com/grupo01/proyectointegrador/Controller/BookingController.java
@@ -0,0 +1,35 @@
+package com.grupo01.proyectointegrador.Controller;
+
+import com.grupo01.proyectointegrador.Model.Booking;
+import com.grupo01.proyectointegrador.Model.Characteristic;
+import com.grupo01.proyectointegrador.Service.BookingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+public class BookingController {
+
+    @Autowired
+    BookingService bookingService;
+
+    @PostMapping("/insert")
+    public ResponseEntity<Booking> guardar(@RequestBody Booking booking) throws Exception {
+        return ResponseEntity.ok(bookingService.guardar(booking));
+    }
+
+    @GetMapping("/findById/{id}")
+    public ResponseEntity<Booking> buscarId(@PathVariable Long id) throws Exception{
+        return ResponseEntity.ok(bookingService.buscarId(id));
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> borrar(@PathVariable Long id) throws Exception {
+        bookingService.borrar(id);
+        return ResponseEntity.ok("Reserva borrada con exito");
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<Booking> actualizar(@RequestBody Booking booking) throws Exception {
+        return ResponseEntity.ok(bookingService.guardar(booking));
+    }
+}

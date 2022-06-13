8e27521ae36d7461f00aa10a3144f6f550b36088
Ana Olivares <anaolivares2712@gmail.com>
2022-06-13 18:22:31 -0300

    [ADD] Agrego Role Service

---
+++ backend/proyectointegrador/src/main/java/com/grupo01/proyectointegrador/Service/RoleService.java
@@ -0,0 +1,42 @@
+package com.grupo01.proyectointegrador.Service;
+
+import com.grupo01.proyectointegrador.Model.Role;
+import com.grupo01.proyectointegrador.Repository.IRoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Optional;
+
+public class RoleService {
+
+    @Autowired
+    IRoleRepository roleRepository;
+
+    public Role guardar(Role role) throws Exception {
+        return roleRepository.save(role);
+    }
+
+    public Role buscarId(Long id) throws Exception {
+        Optional<Role> role = roleRepository.findById(id);
+        return role.orElse(null);
+    }
+
+    public void borrar (Long id) throws Exception {
+        Role role = buscarId(id);
+        if(role != null) {
+            roleRepository.deleteById(id);
+        }
+        else{
+            throw new Exception("Rol con id: "+ id + " no existe");
+        }
+    }
+
+    public Role actualizar (Role role) throws Exception {
+        Role role1 = buscarId(role.getId());
+        if (role1 != null){
+            return roleRepository.save(role);
+        }
+        else{
+            throw new Exception("Rol con id: "+ role.getId() + " no existe");
+        }
+    }
+}

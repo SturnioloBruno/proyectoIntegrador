5377983623c5904cffd6bb6333bbf63920f73f23
Ana Olivares <anaolivares2712@gmail.com>
2022-06-14 18:28:38 -0300

    [FIX] Arreglo Role Service

--- backend/proyectointegrador/src/main/java/com/grupo01/proyectointegrador/Service/RoleService.java
+++ backend/proyectointegrador/src/main/java/com/grupo01/proyectointegrador/Service/RoleService.java
@@ -3,9 +3,11 @@ package com.grupo01.proyectointegrador.Service;
 import com.grupo01.proyectointegrador.Model.Role;
 import com.grupo01.proyectointegrador.Repository.IRoleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+@Service
 public class RoleService {
 
     @Autowired

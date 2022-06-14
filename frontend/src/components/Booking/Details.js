7092abc0c1d4324b6fe4eef666c9cd589f734e36
unknown <gianna.belen.russo@gmail.com>
2022-06-14 01:02:13 -0300

    [ADD] Tablet - Implementar el formulario de reserva

--- frontend/src/components/Booking/Details.js
+++ frontend/src/components/Booking/Details.js
@@ -9,25 +9,27 @@ function Details({ src, name, category, address, city, stars }) {
         <section className="section__booking-details">
             <div>
                 <h2>Detalle de la reserva</h2>
-                <img src={src} alt={name} />
-                <div>
-                    <p className="p__category-name">{category}</p>
-                    <h3>{name}</h3>
-                    <Stack spacing={1}>
-                        <Rating name="half-rating-read" size="small" defaultValue={stars + .0} value={stars + .0} precision={0.5} emptyIcon={<StarIcon fontSize="inherit" style={{ opacity: 0.5 }} />} readOnly />
-                    </Stack>
-                    <p className="p__accommodation-direction">{address + ", " + city}</p>
-                    <ul>
-                        <li>
-                            <h4>Check in</h4>
-                            <span>-/-/-</span>
-                        </li>
-                        <li>
-                            <h4>Check out</h4>
-                            <span>-/-/-</span>
-                        </li>
-                    </ul>
-                    <Link to="/ok" className="btn button__solid-type">Confirmar reserva</Link>
+                <div className="div__hotel-details">
+                    <img src={src} alt={name} />
+                    <div>
+                        <p className="p__category-name">{category}</p>
+                        <h3>{name}</h3>
+                        <Stack spacing={1}>
+                            <Rating name="half-rating-read" size="small" defaultValue={stars + .0} value={stars + .0} precision={0.5} emptyIcon={<StarIcon fontSize="inherit" style={{ opacity: 0.5 }} />} readOnly />
+                        </Stack>
+                        <p className="p__accommodation-direction">{address + ", " + city}</p>
+                        <ul>
+                            <li>
+                                <h4>Check in</h4>
+                                <span>-/-/-</span>
+                            </li>
+                            <li>
+                                <h4>Check out</h4>
+                                <span>-/-/-</span>
+                            </li>
+                        </ul>
+                        <Link to="/ok" className="btn button__solid-type">Confirmar reserva</Link>
+                    </div>
                 </div>
             </div>
         </section>

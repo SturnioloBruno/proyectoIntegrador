7dcef8e6d64c8c38c36aed86fc45fd33834a597c
nicolasmartingutierrez@gmail.com <nicolasmartingutierrez@gmail.com>
2022-06-14 20:28:03 -0300

    [ADD] UseDTO

---
+++ backend/proyectointegrador/src/main/java/com/grupo01/proyectointegrador/DTO/UserDTO.java
@@ -0,0 +1,54 @@
+package com.grupo01.proyectointegrador.DTO;
+
+public class UserDTO {
+    private Long id;
+    private String userName;
+    private String userSurname;
+    private String userEmail;
+    private String userPassword;
+    private String userCity;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserSurname() {
+        return userSurname;
+    }
+
+    public void setUserSurname(String userSurname) {
+        this.userSurname = userSurname;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public String getUserCity() {
+        return userCity;
+    }
+
+    public void setUserCity(String userCity) {
+        this.userCity = userCity;
+    }
+}

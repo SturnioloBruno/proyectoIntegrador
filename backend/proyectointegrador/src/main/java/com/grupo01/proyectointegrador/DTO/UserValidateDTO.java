a92a9cdc9061fe60d98636dbe6787219e3562bd6
nicolasmartingutierrez@gmail.com <nicolasmartingutierrez@gmail.com>
2022-06-15 13:58:19 -0300

    [ADD] UserValidateDTO

---
+++ backend/proyectointegrador/src/main/java/com/grupo01/proyectointegrador/DTO/UserValidateDTO.java
@@ -0,0 +1,35 @@
+package com.grupo01.proyectointegrador.DTO;
+
+public class UserValidateDTO {
+
+    private String userName;
+    private String userSurname;
+    private String userEmail;
+
+    public UserValidateDTO() {
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserSurname() {
+        return userSurname;
+    }
+
+    public void setUserSurname(String userSurname) {
+        this.userSurname = userSurname;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+}

d217c33893d7299b824bf61f21093c77b0743251
nicolasmartingutierrez@gmail.com <nicolasmartingutierrez@gmail.com>
2022-06-14 21:21:43 -0300

    [FIX] UserService con UserDTO

--- backend/proyectointegrador/src/main/java/com/grupo01/proyectointegrador/Service/UserService.java
+++ backend/proyectointegrador/src/main/java/com/grupo01/proyectointegrador/Service/UserService.java
@@ -1,5 +1,7 @@
 package com.grupo01.proyectointegrador.Service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.grupo01.proyectointegrador.DTO.UserDTO;
 import com.grupo01.proyectointegrador.Model.User;
 import com.grupo01.proyectointegrador.Repository.IUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,18 +15,43 @@ public class UserService {
     @Autowired
     IUserRepository userRepository;
 
-    public User guardar(User user) throws Exception {
-        return userRepository.save(user);
+    @Autowired
+    ObjectMapper mapper;
+
+
+    public void crearUser(User user) throws Exception {
+        userRepository.save(user);
+    }
+
+
+    public UserDTO buscarPorIdDT0(Long id) throws Exception{
+        Optional<User> user = userRepository.findById(id);
+        UserDTO userEncontrado = null;
+        if (user.isPresent()){
+            userEncontrado = mapper.convertValue(user.get(),UserDTO.class);
+            userEncontrado.setUserName(user.get().getUserName());
+            userEncontrado.setUserSurname(user.get().getUserSurname());
+            userEncontrado.setUserEmail(user.get().getUserEmail());
+            userEncontrado.setUserPassword(user.get().getUserPassword());
+            userEncontrado.setUserCity(user.get().getUserCity());
+
+
+        }
+            return userEncontrado;
     }
 
     public User buscarId(Long id) throws Exception {
         Optional<User> user = userRepository.findById(id);
+
         return user.orElse(null);
     }
 
     public void borrar (Long id) throws Exception {
-        User user = buscarId(id);
+
+        UserDTO user = buscarPorIdDT0(id);
+
         if(user != null) {
+
             userRepository.deleteById(id);
         }
         else{
@@ -32,13 +59,14 @@ public class UserService {
         }
     }
 
-    public User actualizar (User user) throws Exception {
-        User user1 = buscarId(user.getId());
-        if (user1 != null){
-            return userRepository.save(user);
+    public UserDTO actualizar (UserDTO userDTO) throws Exception {
+        UserDTO userBuscado = buscarPorIdDT0(userDTO.getId());
+
+        if(userBuscado == null){
+           throw new Exception("Usuario con id: "+ userDTO.getId() + " no existe");
         }
-        else{
-            throw new Exception("Usuario con id: "+ user.getId() + " no existe");
-        }
+        User user=mapper.convertValue(userDTO,User.class);
+        userRepository.save(user);
+        return userDTO;
     }
 }

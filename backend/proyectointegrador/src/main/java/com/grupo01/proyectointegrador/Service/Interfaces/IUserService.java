42261498edd817cdca8cd89ce43176a57a6a6390
nicolasmartingutierrez@gmail.com <nicolasmartingutierrez@gmail.com>
2022-06-14 21:20:58 -0300

    [ADD] Interface User

---
+++ backend/proyectointegrador/src/main/java/com/grupo01/proyectointegrador/Service/Interfaces/IUserService.java
@@ -0,0 +1,15 @@
+package com.grupo01.proyectointegrador.Service.Interfaces;
+
+import com.grupo01.proyectointegrador.DTO.UserDTO;
+import com.grupo01.proyectointegrador.Model.User;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface IUserService {
+    void crearUser(User user) throws Exception;
+    List<UserDTO> getUsers(Optional<Boolean> sort) throws Exception;
+    UserDTO buscarPorIdDTO(Long id) throws Exception;
+    void delete(Long id) throws Exception;
+    UserDTO update(UserDTO userDTO) throws Exception;
+}

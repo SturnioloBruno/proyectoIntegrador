dad7618fa28a7df84a15f38402cffd8973589318
unknown <gianna.belen.russo@gmail.com>
2022-06-15 14:45:12 -0300

    [ADD] Lista de horarios

--- frontend/src/components/Booking/Hour.js
+++ frontend/src/components/Booking/Hour.js
@@ -1,6 +1,33 @@
 import "../../styles/Booking/Hour.css";
 
 function Hour() {
+    const hours = [
+        { "value": 0, "hour": "00:00AM" },
+        { "value": 1, "hour": "01:00AM" },
+        { "value": 2, "hour": "02:00AM" },
+        { "value": 3, "hour": "03:00AM" },
+        { "value": 4, "hour": "04:00AM" },
+        { "value": 5, "hour": "05:00AM" },
+        { "value": 6, "hour": "06:00AM" },
+        { "value": 7, "hour": "07:00AM" },
+        { "value": 8, "hour": "08:00AM" },
+        { "value": 9, "hour": "09:00AM" },
+        { "value": 10, "hour": "10:00AM" },
+        { "value": 11, "hour": "11:00AM" },
+        { "value": 12, "hour": "12:00AM" },
+        { "value": 13, "hour": "13:00AM" },
+        { "value": 14, "hour": "14:00AM" },
+        { "value": 15, "hour": "05:00AM" },
+        { "value": 16, "hour": "16:00AM" },
+        { "value": 17, "hour": "17:00AM" },
+        { "value": 18, "hour": "18:00AM" },
+        { "value": 19, "hour": "19:00AM" },
+        { "value": 20, "hour": "20:00AM" },
+        { "value": 21, "hour": "21:00AM" },
+        { "value": 22, "hour": "22:00AM" },
+        { "value": 23, "hour": "23:00AM" },
+    ]
+
     return (
         <section className="section__booking-hour">
             <h2>Tu horario de llegada</h2>
@@ -10,9 +37,9 @@ function Hour() {
                     <span>Indicá tu horario estimado de llegada</span>
                     <select name="hour" required>
                         <option defaultValue={"Seleccionar hora"}>Seleccionar hora</option>
-                        <option value="00">00:00AM</option>
-                        <option value="01">01:00AM</option>
-                        <option value="02">02:00AM</option>
+                        {hours.map((hour, i) => {
+                            return <option key={i} value={hour.value}>{hour.hour}</option>;
+                        })}
                     </select>
                 </label>
             </div>

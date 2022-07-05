4c0cca3e75d6686b633eae5012ba121e89920e34
unknown <gianna.belen.russo@gmail.com>
2022-07-05 01:58:38 -0300

    [DEL] Menú admin + user - Modal.js

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,6 +1,5 @@
 import { useContext ,useEffect, useState} from 'react';
 import Title from "./Title";
-import Modal from "./Modal";
 import '../styles/Header.css';
 import { Link } from "react-router-dom";
 import { UserContext } from './Context/UserContext';

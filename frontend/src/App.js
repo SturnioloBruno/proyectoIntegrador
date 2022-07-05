75beeb14cc3f6315d861bf41cf9524839aa8c08f
unknown <gianna.belen.russo@gmail.com>
2022-07-05 14:43:52 -0300

    [ADD] Permisos de administrador

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,6 +17,8 @@ import BookingList from "./components/Booking/BookingList";
 import FavouritesList from './components/Favourites/FavouritesList';
 
 function App() {
+  const role = localStorage.getItem("user") ? JSON.parse(localStorage.getItem("user")).role.roleName : "";
+
   return (
     <Fragment>
       <Meta/>
@@ -35,7 +37,7 @@ function App() {
         <Route path="/admin/register" element={<Register type="register" />} />
         <Route path="/product/:id/booking" element={<Booking type="booking" />} />
         <Route path="/booking-ok" element={<SuccessfulBooking type="booking-ok" />} />
-        <Route path="/administration" element={<CreateHotel type="create" />} />
+        {role == "ADMIN" ? <Route path="/administration" element={<CreateHotel type="create" />} /> : <Route path="/administration" element={<SuccessfulBooking type="denegate" />} />}
         <Route path="/create-ok" element={<SuccessfulBooking type="create-ok" />} />
         <Route path="/my-bookings" element={<BookingList type="booking-list" />} />
         <Route path="/accountconfirmation/:id" element={<SuccessfulBooking type="confimation-ok" />} />

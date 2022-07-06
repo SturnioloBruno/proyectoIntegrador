6be076bc39bfc714e88888fac04058e970ba3c80
unknown <gianna.belen.russo@gmail.com>
2022-07-06 02:36:18 -0300

    [ADD][FIX] Mensajes de error

--- frontend/src/components/Products/HotelDate.js
+++ frontend/src/components/Products/HotelDate.js
@@ -1,20 +1,20 @@
+import { React, useState } from 'react';
 import { Link } from "react-router-dom";
 import Calendar from "./Calendar";
 
 function HotelDate({id,bookings}) {
+    const [pRange, setpRange] = useState(null);
+
     if(!localStorage.getItem("user")) {
         sessionStorage.setItem("url", `/product/${id}/booking`);
         sessionStorage.setItem("msg", "Debe iniciar sesión para realizar reservas.");
     }
 
-    const handlerClick = (e)=>{
+    const handlerClick = (e) => {
         if(sessionStorage.getItem("dateStart") === null || sessionStorage.getItem("dateEnd") === null) {
-            alert("Seleccione un rango de fechas para continuar.");
-        }
-
-        if(!sessionStorage.getItem("dateStart")&&!sessionStorage.getItem("dateEnd")){
             e.preventDefault();
-            return //VER DE MOSTRAR ALGO
+            let error = "Seleccione un rango de fechas para continuar.";
+            setpRange(error);
         }
     }
 
@@ -25,6 +25,7 @@ function HotelDate({id,bookings}) {
                 <Calendar bookings={bookings}/>
                 <div className="div__button-selected">
                     <p className="p__bold-style">Agregá tus fechas de viaje para obtener precios exactos</p>
+                    {pRange != null ? <p className='p__error'>{pRange}</p> : ""}
                     {localStorage.user === undefined ?
                     <Link onClick={handlerClick} to={`/login`} className="btn button__solid-type">Iniciar reserva</Link> :
                     <Link onClick={handlerClick} to={`/product/${id}/booking`} className="btn button__solid-type">Iniciar reserva</Link> }
